600b74f62579e80eac118613757575cc226f3e52
Greta Ra <ggretute88@gmail.com>
2023-06-20 16:13:12 +0200

    Change project

--- src/components/ProjectsGrid.js
+++ src/components/ProjectsGrid.js
@@ -1,7 +1,6 @@
 import Project from "./Project";
 import memoryGame from "../images/memoryGame.png";
 import weatherAppScreenshot from "../images/weatherapp.png";
-import rockPaperScissorsScreenshot from "../images/rockPaperScissors.png";
 import Fylo from "../images/fylo.png";
 
 const ProjectsGrid = () => {
@@ -14,29 +13,29 @@ const ProjectsGrid = () => {
 					imgSrc={Fylo}
 					title={"FYLO LANDING PAGE"}
 					description={
-						"A landing page for a fictional company. Made using React and Tailwind."
+						"A landing page for a fictional company. Made using React and styled with Tailwind."
 					}
 					github={"https://github.com/GretaRa/fylo"}
 					liveDemo={"https://gretara.github.io/fylo/"}
 				/>
 				<Project
-					imgSrc={memoryGame}
-					title={"MEMORY CARDS GAME"}
+					imgSrc={weatherAppScreenshot}
+					title={"WEATHER APP"}
 					description={
-						"A simple memory game with cards featuring plant pictures. The game is made with React as a part of The Odin Project curriculum."
+						"A weather app that shows the current weather and a 4-day forecast. Made with React and styled with Tailwind."
 					}
-					github={"https://github.com/GretaRa/memory-game"}
-					liveDemo={"https://gretara.github.io/memory-game/"}
+					github={"https://github.com/GretaRa/weather-app"}
+					liveDemo={"https://gretara.github.io/weather-app/"}
 					reverse="lg:flex-row-reverse"
 				/>
 				<Project
-					imgSrc={rockPaperScissorsScreenshot}
-					title={"ROCK PAPER SCISSORS"}
+					imgSrc={memoryGame}
+					title={"MEMORY CARDS GAME"}
 					description={
-						"A rock paper scissors game against a computer. Made with vanilla JavaScript."
+						"A simple memory game with cards featuring plant pictures. Made with React."
 					}
-					github={"https://github.com/GretaRa/RockPaperScissors"}
-					liveDemo={"https://gretara.github.io/RockPaperScissors/"}
+					github={"https://github.com/GretaRa/memory-game"}
+					liveDemo={"https://gretara.github.io/memory-game/"}
 				/>
 			</div>
 		</div>
