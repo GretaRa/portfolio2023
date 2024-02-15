4636930d1b0fddc1d515220e6e83d9bf9849f1cb
Greta Ra <ggretute88@gmail.com>
2024-02-15 14:37:21 +0100

    Refactor project display

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,19 +1,19 @@
 import { AiFillGithub, AiOutlineRocket } from "react-icons/ai";
 
-const Project = ({ reverse, imgSrc, title, description, github, liveDemo }) => {
+const Project = ({ imgSrc, title, description, github, liveDemo, style }) => {
 	return (
 		<div
-			className={`border-2 border-black flex flex-col items-center lg:items-stretch lg:gap-12 gap-5 ${reverse} lg:flex-row mx-auto p-5 rounded`}
+			className={`${style} border-2 border-black flex flex-col items-center lg:items-stretch lg:gap-12 gap-5  lg:flex-row mx-auto p-5 rounded`}
 		>
 			<a href={liveDemo} target="_blank" rel="noreferrer">
 				<img
 					className=" w-[37rem] max-h-80 md:h-80 object-cover rounded shadow-gray-400 border-gray-400 border-2 shadow-md hover:scale-105 ease-in-out duration-1000 object-top hover:object-bottom"
 					src={imgSrc}
-					alt="project website"
+					alt="project website screenshot"
 				/>
 			</a>
 			<div className="flex flex-col gap-1 justify-evenly max-w-xs text-center">
-				<p className="text-lg font-bold">{title.toUpperCase()}</p>
+				<p className="text-lg font-bold">{title}</p>
 				<p className="text-zinc-600">{description}</p>
 				<div className="flex gap-5 justify-center text-lg items-center">
 					<a
--- src/components/ProjectsGrid.js
+++ src/components/ProjectsGrid.js
@@ -1,53 +1,60 @@
 import Project from "./Project";
-import memorygame from "../images/memorygame.png";
-import weatherAppScreenshot from "../images/weatherapp.png";
-import Fylo from "../images/fylo.png";
-import fashionhub from "../images/fashionhub.png";
 
 const ProjectsGrid = () => {
+	const projectList = [
+		{
+			id: 1,
+			title: "Fashionhub",
+			src: require("../images/fashionhub.png"),
+			description:
+				"A simple yet functional online store developed with React.js and styled using Tailwind CSS. It sources product data from the FakeStore API, providing a clean and efficient shopping experience.",
+			github: "https://github.com/GretaRa/FashionHub",
+			demoUrl: "https://fashion-hub-project.vercel.app/",
+		},
+		{
+			id: 2,
+			title: "Weather app",
+			src: require("../images/weatherapp.png"),
+			description:
+				"Web app for visualizing the current weather of a city. It fetches weather data from OpenWeatherMap API and can show current weather based on the user's input or location. Utilizes API integration and Webpack module bundling.",
+			github: "https://github.com/GretaRa/weather-app",
+			demoUrl: "https://gretara.github.io/weather-app/",
+		},
+		{
+			id: 3,
+			title:"Fylo landing page",
+			src: require("../images/fylo.png"),
+			description:
+				"The end product of a front-end challenge that entailed reproducing a specific design for a fictional company's landing page. Built using React and styled with Tailwind.",
+			github: "https://github.com/GretaRa/fylo",
+			demoUrl: "https://gretara.github.io/fylo/",
+		},
+		{
+			id: 4,
+			title:"Memory card game",
+			src: require("../images/memorygame.png"),
+			description:
+			"A simple memory game with cards featuring plant pictures. Made with React.",
+			github: "https://github.com/GretaRa/memory-game",
+			demoUrl: "https://gretara.github.io/memory-game/",
+		},
+	];
+
 	return (
 		<div>
 			<p className="text-2xl lg:text-3xl p-5 text-center">My projects</p>
 			<div className=" grid grid-cols-1 gap-10 m-5">
-				{/* Pass prop reverse="lg:flex-row-reverse" to every other card  */}
-				<Project
-					imgSrc={fashionhub}
-					title={"fashionhub"}
-					description={
-						"A simple yet functional online store developed with React.js and styled using Tailwind CSS. It sources product data from the FakeStore API, providing a clean and efficient shopping experience."
-					}
-					github={"https://github.com/GretaRa/FashionHub"}
-					liveDemo={"https://fashion-hub-project.vercel.app/"}
-				/>
-				<Project
-					imgSrc={weatherAppScreenshot}
-					title={"weather app"}
-					description={
-						"Web app for visualizing the current weather of a city. It fetches weather data from OpenWeatherMap API and can show current weather based on the user's input or location. Utilizes API integration and Webpack module bundling."
-					}
-					github={"https://github.com/GretaRa/weather-app"}
-					liveDemo={"https://gretara.github.io/weather-app/"}
-					reverse="lg:flex-row-reverse"
-				/>
-				<Project
-					imgSrc={Fylo}
-					title={"fylo landing page"}
-					description={
-						"The end product of a front-end challenge that entailed reproducing a specific design for a fictional company's landing page. Built using React and styled with Tailwind."
-					}
-					github={"https://github.com/GretaRa/fylo"}
-					liveDemo={"https://gretara.github.io/fylo/"}
-				/>
-				<Project
-					imgSrc={memorygame}
-					title={"memory card game"}
-					description={
-						"A simple memory game with cards featuring plant pictures. Made with React."
-					}
-					github={"https://github.com/GretaRa/memory-game"}
-					liveDemo={"https://gretara.github.io/memory-game/"}
-					reverse="lg:flex-row-reverse"
-				/>
+				{projectList.map((project) => (
+					<Project 
+					style={project.id % 2 === 0 ?  'lg:flex-row-reverse' : 'lg:flex-row'}
+					key={project.id}
+					imgSrc={project.src} 
+					title={project.title}
+					description={project.description}
+					github={project.github}
+					liveDemo={project.demoUrl}
+					/>
+				))}
 			</div>
 		</div>
 	);
